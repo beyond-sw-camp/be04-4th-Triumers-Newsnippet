70f1e30339e38e8951f11262ce284690a0bc1878
Leegiyeon <rldus3512@naver.com>
2024-04-02 17:40:19 +0900

    docs(Template): 프로젝트 생성, 이슈 및 PR 템플릿 업로드

---
+++ src/main/java/org/triumers/newsnippetback/NewsnippetBackApplication.java
@@ -0,0 +1,13 @@
+package org.triumers.newsnippetback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsnippetBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsnippetBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/triumers/newsnippetback/NewsnippetBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.triumers.newsnippetback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsnippetBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

e3c26c85776ef3a369c1d5c5533a48faea253d9c
Leegiyeon <rldus3512@naver.com>
2024-04-03 16:34:41 +0900

    test(github action): 테스트용 코드 작성

--- src/test/java/org/triumers/newsnippetback/NewsnippetBackApplicationTests.java
+++ src/test/java/org/triumers/newsnippetback/NewsnippetBackApplicationTests.java
@@ -2,12 +2,17 @@ package org.triumers.newsnippetback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class NewsnippetBackApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }

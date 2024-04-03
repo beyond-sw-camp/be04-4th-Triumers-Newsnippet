91114cdaa31634ffe8050a8fe69093de94f71797
Leegiyeon <rldus3512@naver.com>
2024-04-03 17:50:08 +0900

    ci(gradle): gradle 설정 수정

--- src/test/java/org/triumers/newsnippetback/NewsnippetBackApplicationTests.java
+++ src/test/java/org/triumers/newsnippetback/NewsnippetBackApplicationTests.java
@@ -2,13 +2,11 @@ package org.triumers.newsnippetback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class NewsnippetBackApplicationTests {
-
 
 	@Test
 	void contextLoads() {

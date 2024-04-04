19602d17529d29bb68894b9f2d3890bb5261eeb7
hso <sh07202@naver.com>
2024-04-04 11:15:01 +0900

    feat(DTO) : QuizDTO 추가

---
+++ src/main/java/org/triumers/newsnippetback/domain/dto/QuizDTO.java
@@ -0,0 +1,39 @@
+package org.triumers.newsnippetback.domain.dto;
+
+import jakarta.persistence.*;
+import org.triumers.newsnippetback.domain.aggregate.entity.Category;
+import org.triumers.newsnippetback.domain.aggregate.entity.CrawlingQuiz;
+
+import java.time.LocalDate;
+
+public class QuizDTO {
+    private int id;
+
+    private LocalDate date;
+
+    private int no;
+
+    private String content;
+
+    private String optionA;
+
+    private String optionB;
+
+    private String optionC;
+
+    private String optionD;
+
+    private String answer;
+
+    private String explanation;
+
+    private String newsLink;
+
+    private int solvedCnt;
+
+    private int correctCnt;
+
+    private Category category;
+
+    private int originQuizId;
+}

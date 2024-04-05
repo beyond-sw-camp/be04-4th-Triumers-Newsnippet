a9dad9f21fc1dc54b1f351073bd57b6d305cd500
Leegiyeon <rldus3512@naver.com>
2024-04-05 13:58:40 +0900

    style(Quiz): 불필요한 import 삭제

--- src/main/java/org/triumers/newsnippetback/Application/controller/QuizController.java
+++ src/main/java/org/triumers/newsnippetback/Application/controller/QuizController.java
@@ -1,20 +1,15 @@
 package org.triumers.newsnippetback.Application.controller;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.triumers.newsnippetback.Application.service.QuizService;
-import org.triumers.newsnippetback.domain.aggregate.entity.Quiz;
 import org.triumers.newsnippetback.domain.aggregate.vo.QuizRequest;
 import org.triumers.newsnippetback.domain.aggregate.vo.QuizResponse;
 import org.triumers.newsnippetback.domain.dto.QuizDTO;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/quiz")
@@ -22,12 +17,9 @@ public class QuizController {
 
     private final QuizService quizService;
 
-    private final ModelMapper modelMapper;
-
     @Autowired
-    public QuizController(QuizService quizService, ModelMapper modelMapper) {
+    public QuizController(QuizService quizService) {
         this.quizService = quizService;
-        this.modelMapper = modelMapper;
     }
 
     /* 1. 해당 날짜에 출제되는 문제의 카테고리, 지문, 선택지, 정답률 조회
@@ -74,12 +66,4 @@ public class QuizController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
-
-
-
-    /* 3. 회원이 입력한 답과 문제의 정답이 같은지 조회한 후 정답 여부와 회원이 입력한 답 저장 */
-
-
-    /* 4. 문제 id 값 이용해 과거에 풀었던 문제 조회
-    ㄴ 문제의 카테고리, 지문, 선택지, 정답, 선택했던 답, 해설, 원본 링크, 날짜, 정답률 조회 */
 }

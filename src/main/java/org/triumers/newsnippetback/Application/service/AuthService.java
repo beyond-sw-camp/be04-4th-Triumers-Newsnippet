e45135a92a494ba91ffa460ddb691754fc8cb63a
moomint8 <office05053@gmail.com>
2024-04-11 14:46:53 +0900

    refactor(SignupDTO): AuthDTO로 이름 수정에 따른 참조 클래스 수정

--- src/main/java/org/triumers/newsnippetback/Application/service/AuthService.java
+++ src/main/java/org/triumers/newsnippetback/Application/service/AuthService.java
@@ -8,7 +8,7 @@ import org.triumers.newsnippetback.common.exception.UserNicknameDuplicateExcepti
 import org.triumers.newsnippetback.domain.aggregate.entity.User;
 import org.triumers.newsnippetback.domain.aggregate.enums.UserRole;
 import org.triumers.newsnippetback.domain.aggregate.enums.UserStatus;
-import org.triumers.newsnippetback.domain.dto.SignupDTO;
+import org.triumers.newsnippetback.domain.dto.AuthDTO;
 import org.triumers.newsnippetback.domain.repository.UserRepository;
 
 @Service
@@ -23,7 +23,7 @@ public class AuthService {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    public void signup(SignupDTO request) throws UserNicknameDuplicateException, UserEmailDuplicateException {
+    public void signup(AuthDTO request) throws UserNicknameDuplicateException, UserEmailDuplicateException {
 
         // 닉네임 중복 예외 처리
         if (userRepository.existsByNickname(request.getNickname())) {
@@ -48,7 +48,7 @@ public class AuthService {
         return userRepository.existsByEmail(email);
     }
 
-    private User userMapper(SignupDTO request) {
+    private User userMapper(AuthDTO request) {
         User user = new User();
 
         user.setName(request.getName());

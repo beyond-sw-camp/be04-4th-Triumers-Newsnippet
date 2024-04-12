375a71fb1eda93f5cd05044d613e74ec07638209
moomint8 <office05053@gmail.com>
2024-04-12 12:30:28 +0900

    test(Auth): 비밀번호 유효성 테스트 구현
    비밀번호의 유효성을 테스트하는 기능이 구현되었습니다.
    - 정상적인 경우 테스트
    - 8~12자가 아닐 경우 예외 테스트
    - 알파벳, 숫자 이외의 값 예외 테스트
    - 소문자, 대문자, 숫자 각 한 개 이상 미포함 예외 테스트

--- src/test/java/org/triumers/newsnippetback/domain/service/ValidationAuthServiceTest.java
+++ src/test/java/org/triumers/newsnippetback/domain/service/ValidationAuthServiceTest.java
@@ -36,4 +36,33 @@ class ValidationAuthServiceTest {
     void wrongTypeNickname(String nickname) {
         assertThrows(WrongInputTypeException.class, () -> validation.nickname(nickname));
     }
+
+    @DisplayName("비밀번호 정상값 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"aA123456", "1234567890Zz", "aAbBcCdD123"})
+    void rightPassword(String password) throws WrongInputTypeException {
+
+        assertDoesNotThrow(() -> validation.password(password));
+    }
+
+    @DisplayName("비밀번호 길이 예외 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "aAbBcC1", "aAb1234567890"})
+    void wrongLengthPassword(String password) {
+        assertThrows(WrongInputTypeException.class, () -> validation.password(password));
+    }
+
+    @DisplayName("비밀번호 타입 예외 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"aA123456!@#", "테스트123456", "&123aaAAwWd"})
+    void wrongTypePassword(String password) {
+        assertThrows(WrongInputTypeException.class, () -> validation.password(password));
+    }
+
+    @DisplayName("비밀번호 소문자,대문자,숫자 한 개 이상 미포함 예외 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"aa123456", "AA123456", "aAbBcCdDeE"})
+    void wrongIncludePassword(String password) {
+        assertThrows(WrongInputTypeException.class, () -> validation.password(password));
+    }
 }
